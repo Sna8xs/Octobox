e6e61372a3efb84675d60cdec8a93841b48c933c
Sna8xs <felixfstoetzel@gmail.com>
2023-11-01 12:58:33 +0100

    Fixed Rpi.GPIO name in requirements.txt

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/Sna8xs/Octobox"
 plugin_license = "MIT"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ["RPi.GPIO", "threading"]
+plugin_requires = ["RPi.GPIO"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
