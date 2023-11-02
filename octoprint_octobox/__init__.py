a2ed2a1e022783b2e73b1b9ffefb8d3c83274698
Sna8xs <felixfstoetzel@gmail.com>
2023-11-02 10:17:12 +0100

    first half working version

--- octoprint_octobox/__init__.py
+++ octoprint_octobox/__init__.py
@@ -1,21 +1,17 @@
 # Import the necessary modules
-
-# coding=utf-8
 from __future__ import absolute_import
-
-
-import octoprint
+from octoprint.plugin import OctoPrintPlugin
 from octoprint.util import RepeatedTimer
 from octoprint.events import Events
 import logging
-#from LedStripe import LedStripe
+import octoprint
 
-plugin_url = "https://github.com/Sna8xs/Octobox/archive/refs/tags/Beta2.zip"
+# Define the plugin version
+plugin_version = "0.1.0"
 
-class Octobox(octoprint.plugin.OctoPrintPlugin):
+class Octobox(octoprint.plugin.StartupPlugin):
     def __init__(self):
         self._temp_timer = None
-        #self.ledstripe = None
 
     def on_after_startup(self):
         self._logger.info("Octobox started.")
@@ -23,11 +19,9 @@ class Octobox(octoprint.plugin.OctoPrintPlugin):
         nozzle_temp = self._printer.get_current_temperatures()["tool0"]["actual"]
         self._logger.info(f"Bed Temperature: {bed_temp} °C")
         self._logger.info(f"Nozzle Temperature: {nozzle_temp} °C")
-        #self.ledstripe = LedStripe(17, 27, 22)
-
 
     def on_shutdown(self):
-        if self._timer is not None:
+        if self._temp_timer is not None:
             self._temp_timer.cancel()
             self._logger.info("Octobox stopped.")
 
@@ -36,9 +30,9 @@ class Octobox(octoprint.plugin.OctoPrintPlugin):
             self._temp_timer = RepeatedTimer(60, self.get_temperatures)
             self._temp_timer.start()
         if event == Events.PRINT_DONE:
-            #self.ledstripe.print_done()
+            pass
         if event == Events.PRINT_FAILED:
-            #self.ledstripe.print_failed()
+            pass
 
     def get_temperatures(self):
         bed_temp = self._printer.get_current_temperatures()["bed"]["actual"]
@@ -47,39 +41,27 @@ class Octobox(octoprint.plugin.OctoPrintPlugin):
         self._logger.info(f"Bed Temperature: {bed_temp} °C")
         self._logger.info(f"Nozzle Temperature: {nozzle_temp} °C")
 
-
-
     def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-		# for details.
-		return dict(
-			restorelevelingafterg28=dict(
-				displayName="OctoBox",
-				displayVersion=self._plugin_version,
+        # Define the configuration for your plugin to use with the Software Update
+        return dict(
+            octobox=dict(
+                displayName="OctoBox",
+                displayVersion=plugin_version,
+                type="github_release",
+                user="Sna8xs",
+                repo="Octobox",
+                current=plugin_version,
+                pip="https://github.com/Sna8xs/Octobox/archive/refs/tags/Beta2.zip"
+            )
+        )
 
-				# version check: github repository
-				type="github_release",
-				user="Sna8xs",
-				repo="Octobox",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/Sna8xs/Octobox/archive/refs/tags/Beta2.zip"
-			)
-		)
-
-
-__plugin_pythoncompat__ = ">=3.9,<4"
+__plugin_pythoncompat__ = ">=3.0.2,<4"
 
 def __plugin_load__():
     global __plugin_implementation__
     __plugin_implementation__ = Octobox()
-    octoprint.plugin.register_plugin(plugin, __plugin_name__)
 
     global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
-
-
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
